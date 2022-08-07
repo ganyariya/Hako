7e011aacf35ba1e2d0a5b56b29f1f1ad47099ccf
ganariya <ganariya2525@gmail.com>
2022-08-07 16:45:17 +0900

    :rotating_light: fix linter

--- tests/hogame/Controller/User/GetsController.php
+++ tests/hogame/Controller/User/GetsController.php
@@ -16,12 +16,14 @@ final class GetsController
         $this->interactor = $interactor;
     }
 
-    // 本当は \Psr\RequestInterface 系を継承したもの
+    /**
+     * @param string $userId
+     * @return mixed[]
+     */
     public function __invoke(string $userId): array
     {
         $inputData = new GetsInputData(["userId" => $userId]);
         $response = $this->interactor->handle($inputData);
-        // 本当は presenter を挟んだほうが良い
         return $response->toArray();
     }
 }
